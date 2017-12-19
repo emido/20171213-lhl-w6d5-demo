d34ae6371b6a15b70dac84b9af5b4ccd8590c1a4
Roland Tecson <roland_tecson@hotmail.com>
2017-12-19 01:56:43 -0800

    Initial Commit

---
+++ w6d5/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  w6d5
+//
+//  Created by Roland on 2017-12-19.
+//  Copyright © 2017 Lighthouse Labs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
